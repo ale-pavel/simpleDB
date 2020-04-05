8f504bf2e881aaaed0e0dd2be586eaaf76073b99
Alexandru Pavel <alexandru.pavel.94@gmail.com>
2020-04-05 21:50:38 +0200

    Provides an implementation of TimedBuffer
    - Local variables are firstPinned (int) and buffer (Buffer)
    - Getter and setter methods
    - Equals and hashcode methods are based only on the buffer local
    variable, not taking firstPinned into account. This is necessary because
    otherwise it couldn't be possible to check if the tree contains the
    buffer if there are multiple pins at different times.

--- src/simpledb/buffer/TimedBuffer.java
+++ src/simpledb/buffer/TimedBuffer.java
@@ -3,4 +3,37 @@ package simpledb.buffer;
 public class TimedBuffer {
 	private int firstPinned;
 	private Buffer buffer;
+	
+	public TimedBuffer(int firstPinned, Buffer buffer) {
+		super();
+		this.firstPinned = firstPinned;
+		this.buffer = buffer;
+	}
+
+	public int getFirstPinned() {
+		return firstPinned;
+	}
+
+	public void setFirstPinned(int firstPinned) {
+		this.firstPinned = firstPinned;
+	}
+
+	public Buffer getBuffer() {
+		return buffer;
+	}
+
+	public void setBuffer(Buffer buffer) {
+		this.buffer = buffer;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.getBuffer().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object buff) {
+		return this.getBuffer().equals(buff);
+	}
+		
 }
