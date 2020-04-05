4fb0b826a0347f2b4d8e378c0afb34465f78f6be
Alexandru Pavel <alexandru.pavel.94@gmail.com>
2020-04-05 22:26:18 +0200

    Implements FIFO strategy
    - Uses eventTimer to track the event sequence and fill the tree with
    firstPinned values
    - pinned() check is the buffer is already in the tree, if not it adds it
    with the current timer
    - unpinned() only increments the eventTimer
    - chooseUnpinnedBuffer() traverses the tree to find the first unpinned
    buffer in ascending order (based on firstPinned value as specified in
    the Comparator)

--- src/simpledb/buffer/FIFOUnpinStrategy.java
+++ src/simpledb/buffer/FIFOUnpinStrategy.java
@@ -5,27 +5,48 @@ import java.util.TreeSet;
 public class FIFOUnpinStrategy implements ChooseUnpinnedBufferStrategy, PinUnpinListener {
 	private Buffer[] bufferPool;
 	private TreeSet<TimedBuffer> bufferQueue;
+	//Needed to assign firstPinned values to TimedBuffers objects. Increased on pin/unpin events
+	private int eventTimer;
 
 	public void create(Buffer[] bufferPool) {
-
+		this.bufferPool = bufferPool;
+		bufferQueue = new TreeSet<TimedBuffer>();
+		eventTimer = 1;
+	}
+	
+	private void incrementTimer() {
+		eventTimer++;
 	}
 
 	@Override
 	public void pinned(Buffer b) {
-		// TODO Auto-generated method stub
-
+		TimedBuffer timedBuff = new TimedBuffer(eventTimer, b);
+		//If the TimedBuffer is already in the tree it shouldn't be added again
+		if(!bufferQueue.contains(timedBuff))
+			bufferQueue.add(timedBuff);
+		incrementTimer();
 	}
 
 	@Override
 	public void unpinned(Buffer b) {
-		// TODO Auto-generated method stub
-
+		//Unpinned events do not affect FIFO strategy, unpinned blocks are searched in chooseUnpinnedBuffer() method
+		incrementTimer();
 	}
 
 	@Override
 	public Buffer chooseUnpinnedBuffer() {
-		// TODO Auto-generated method stub
-		return null;
+		Buffer unpinnedBuffer = null;
+		if(!bufferQueue.isEmpty()) {
+			//The lowest value in the tree is the first pinned buffer
+			for(TimedBuffer tb: bufferQueue) {
+				if(!tb.getBuffer().isPinned()) {
+					//currentBuffer is the lowest unpinned buffer in the tree
+					unpinnedBuffer = tb.getBuffer();
+					break;
+				}
+			}
+		}
+		return unpinnedBuffer;
 	}
 
 }
