e9825fb7ce2efc52da4bbb7fac46d9bfe5f8d601
Alexandru Pavel <alexandru.pavel.94@gmail.com>
2020-04-05 19:02:33 +0200

    Implements LRU strategy using a Deque (LinkedList)
    - startup method to fill the queue
    - pin/unpin removes/adds a buffer to the queue
    - chooseUnpinnedBuffer polls the first element (less recent)

--- src/simpledb/buffer/LRUUnpinStrategy.java
+++ src/simpledb/buffer/LRUUnpinStrategy.java
@@ -1,28 +1,44 @@
 package simpledb.buffer;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class LRUUnpinStrategy implements ChooseUnpinnedBufferStrategy, PinUnpinListener {
 	private Buffer[] bufferPool;
+	//Handles the unpinned buffers based on the sequence of unpin events arrived
+	private Deque<Buffer> unpinnedBufferQueue;
 
 	public void create(Buffer[] bufferPool) {
-
+		this.bufferPool = bufferPool;
+		unpinnedBufferQueue = new LinkedList<Buffer>();
+		//Adds all the unpinned buffers to the queue (all should be during create)
+		unpinnedQueueStartup();
+	}
+	
+	private void unpinnedQueueStartup() {
+		for(Buffer b : bufferPool) {
+			if(!b.isPinned())
+				unpinnedBufferQueue.addLast(b);
+		}
 	}
 
 	@Override
 	public void pinned(Buffer b) {
-		// TODO Auto-generated method stub
-
+		//The buffer b is not available anymore to be pinned (or wasn't already)
+		unpinnedBufferQueue.remove(b);
 	}
 
 	@Override
 	public void unpinned(Buffer b) {
-		// TODO Auto-generated method stub
-
+		//Adds the recently unpinned buffer to the tail of the queue
+		if(!b.isPinned() && !unpinnedBufferQueue.contains(b))
+			unpinnedBufferQueue.addLast(b);
 	}
 
 	@Override
 	public Buffer chooseUnpinnedBuffer() {
-		// TODO Auto-generated method stub
-		return null;
+		//The first buffer was unpinned less recently
+		return unpinnedBufferQueue.pollFirst();
 	}
 
 }
