e0dc4ceffa752777ecd6a49ea1ae9ecee16db495
Alexandru Pavel <alexandru.pavel.94@gmail.com>
2020-04-05 20:43:06 +0200

    Implements Clock strategy (with a Set)
    - INFO: Use of Deque structure was not needed (used Set instead)
    - The Set keeps track of the current unpinned buffers
    - On pinned event the buffer position is obtained and currentPosition is
    updated with it (if the buffer was previously unpinned in the set)
    - On unpinned the set will have the buffer added to it (if the buffer is
    now unpinned in the bufferPool)

--- src/simpledb/buffer/ClockUnpinStrategy.java
+++ src/simpledb/buffer/ClockUnpinStrategy.java
@@ -1,32 +1,81 @@
 package simpledb.buffer;
 
-import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+//import java.util.Deque;
 
 public class ClockUnpinStrategy implements ChooseUnpinnedBufferStrategy, PinUnpinListener {
 	private Buffer[] bufferPool;
 	private int currentPosition;
-	private Deque<Buffer> unpinnedBufferQueue;
+	//private Deque<Buffer> unpinnedBufferQueue;
+	private Set<Buffer> unpinnedBufferSet;
 
 	public void create(Buffer[] bufferPool) {
-
+		this.bufferPool = bufferPool;
+		//Starts scanning at the first element
+		this.currentPosition = 0;
+		//Keeps track of the unpinned buffer to change currentPosition correctly during pinned events
+		this.unpinnedBufferSet = new HashSet<Buffer>();
+		//Fills the set with unpinned buffers (should be all during create)
+		unpinnedSetStartup();
+	}
+	
+	private void unpinnedSetStartup() {
+		for(Buffer b : bufferPool)
+			if(!b.isPinned())
+				unpinnedBufferSet.add(b);
+	}
+	
+	//Lookup of a buffer in the bufferPool, returning its position
+	private int bufferIndex(Buffer b) {
+		for(int i=0; i<bufferPool.length; i++) {
+			if(bufferPool[i].equals(b))
+				return i;
+		}
+		return -1;
 	}
 
 	@Override
 	public void pinned(Buffer b) {
-		// TODO Auto-generated method stub
-
+		//The buffer was unpinned and is now pinned (a replacement has occurred)
+		if(unpinnedBufferSet.contains(b)) {
+			//Update with the index of the replaced buffer
+			this.currentPosition = bufferIndex(b);
+			unpinnedBufferSet.remove(b);
+		}
 	}
 
 	@Override
 	public void unpinned(Buffer b) {
-		// TODO Auto-generated method stub
-
+		//If the buffer has been totally unpinned add it to the set tracking unpinned buffers
+		if(!b.isPinned())
+			unpinnedBufferSet.add(b);
 	}
 
 	@Override
 	public Buffer chooseUnpinnedBuffer() {
-		// TODO Auto-generated method stub
-		return null;
+		Buffer chosenBuffer = null;
+		boolean foundUnpinned = false;
+		boolean finishedScan = false;
+		//Start scanning from the next element of the last replacement
+		int i = currentPosition + 1;
+		
+		while(!foundUnpinned || !finishedScan) {			
+			if(i < bufferPool.length) {
+				if(!bufferPool[i].isPinned()) {
+					//Found the first free buffer from the last replacement
+					chosenBuffer = bufferPool[i];
+					foundUnpinned = true;
+				}
+				i++;
+			} else if(i == currentPosition) {
+				//The circular scan ended
+				finishedScan = true;
+			} else
+				//Resume the lookup (not finished) from the start of the bufferPool
+				i = 0;
+		}
+		return chosenBuffer;
 	}
 
 }
